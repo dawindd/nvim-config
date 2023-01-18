487c253ff5cc7b73088001ccdb15fb908533c618
Benjamin Betti <benjamin@betti.me>
2023-01-18 14:20:38 +0100

    feat(ts): add some common servers

--- lua/user/parsers.lua
+++ lua/user/parsers.lua
@@ -1,11 +1,26 @@
 return {
-	-- Basics
+	-- Development basics
+	"bash",
 	"diff",
+	"dockerfile",
 	"git_rebase",
 	"gitcommit",
 	"gitignore",
+	"json",
+	"markdown",
+	"regex",
+	"yaml",
 	-- Neovim development
 	"help",
 	"lua",
 	"query",
+	-- Dotfiles
+	"fish",
+	"rasi",
+	"sxhkdrc",
+	"toml",
+	-- Web development
+	"css",
+	"html",
+	"javascript",
 }
