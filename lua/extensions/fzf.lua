0fe11d8f7f3fcda6407180b323dca467ea4b275c
Benjamin Betti <benjamin@betti.me>
2023-01-21 14:41:56 +0100

    refactor(lsp): use fzf-lua more often

--- lua/extensions/fzf.lua
+++ lua/extensions/fzf.lua
@@ -1,37 +1,29 @@
 Ensure("fzf-lua", function(fzf)
-	vim.keymap.set({ "n", "v" }, "<leader><leader>f", fzf.files, {
+	vim.keymap.set("n", "<leader><leader>f", fzf.files, {
 		desc = "Fuzzy-find files in cwd",
 	})
-
-	vim.keymap.set({ "n", "v" }, "<leader><leader>o", fzf.oldfiles, {
+	vim.keymap.set("n", "<leader><leader>o", fzf.oldfiles, {
 		desc = "Fuzzy-find files in history",
 	})
-
-	vim.keymap.set({ "n", "v" }, "<leader><leader>h", fzf.help_tags, {
+	vim.keymap.set("n", "<leader><leader>h", fzf.help_tags, {
 		desc = "Fuzzy-find help tags",
 	})
-
-	vim.keymap.set({ "n", "v" }, "<leader><leader>g", fzf.git_status, {
+	vim.keymap.set("n", "<leader><leader>g", fzf.git_status, {
 		desc = "Fuzzy-find files in git status",
 	})
-
-	vim.keymap.set({ "n", "v" }, "<leader><leader>l", fzf.loclist, {
-		desc = "Open location list",
+	vim.keymap.set("n", "<leader><leader>l", fzf.diagnostics_workspace, {
+		desc = "Open workspace diagnostics ",
 	})
-
-	vim.keymap.set({ "n", "v" }, "<leader><leader>q", fzf.quickfix, {
-		desc = "Open quickfix list",
+	vim.keymap.set("n", "<leader><leader>q", fzf.diagnostics_document, {
+		desc = "Open buffer diagnostics",
 	})
-
-	vim.keymap.set({ "n", "v" }, "<leader><leader>j", fzf.jumps, {
+	vim.keymap.set("n", "<leader><leader>j", fzf.jumps, {
 		desc = "Open jump list",
 	})
-
-	vim.keymap.set({ "n", "v" }, "<leader><leader>b", fzf.buffers, {
+	vim.keymap.set("n", "<leader><leader>b", fzf.buffers, {
 		desc = "Fuzzy-find buffers",
 	})
-
-	vim.keymap.set({ "n", "v" }, "<leader><leader>:", fzf.live_grep_native, {
+	vim.keymap.set("n", "<leader><leader>:", fzf.live_grep_native, {
 		desc = "Fuzzy-search project",
 	})
 end)
--- lua/lsp/keybinds.lua
+++ lua/lsp/keybinds.lua
@@ -1,66 +1,73 @@
 local M = {}
 
 function M.setup(bufnr)
-	-- LSP
-	vim.keymap.set({ "n" }, "<leader>ld", vim.lsp.buf.definition, {
-		buffer = bufnr,
-		desc = "LSP - Show definition",
-	})
-	vim.keymap.set({ "n" }, "<leader>lk", vim.lsp.buf.hover, {
+	Ensure("fzf-lua", function(fzf)
+		-- LSP
+		vim.keymap.set("n", "<leader>ld", fzf.lsp_definitions, {
+			buffer = bufnr,
+			desc = "LSP - Show definitions",
+		})
+		vim.keymap.set("n", "<leader>lu", fzf.lsp_references, {
+			buffer = bufnr,
+			desc = "LSP - Show usages",
+		})
+		vim.keymap.set("n", "<leader>lt", fzf.lsp_typedefs, {
+			buffer = bufnr,
+			desc = "LSP - Show type definition",
+		})
+		vim.keymap.set("n", "<leader>li", fzf.lsp_implementations, {
+			buffer = bufnr,
+			desc = "LSP - Show implementation",
+		})
+		vim.keymap.set("n", "<leader><leader><leader>",
+			fzf.lsp_live_workspace_symbols,
+			{ desc = "Fuzzy-find workspace symbols" }
+		)
+		vim.keymap.set("n", "<leader>la", fzf.lsp_code_actions, {
+			buffer = bufnr,
+			desc = "LSP - Code actions",
+		})
+	end)
+
+	vim.keymap.set("n", "<leader>lk", vim.lsp.buf.hover, {
 		buffer = bufnr,
 		desc = "LSP - Show documentation",
-	})
-	vim.keymap.set({ "n" }, "<leader>li", vim.lsp.buf.implementation, {
-		buffer = bufnr,
-		desc = "LSP - Show implementation",
 	})
 	vim.keymap.set({ "n", "i" }, "<C-k>", vim.lsp.buf.signature_help, {
 		buffer = bufnr,
 		desc = "LSP - Show function signature",
 	})
-	vim.keymap.set({ "n" }, "<leader>lt", vim.lsp.buf.type_definition, {
-		buffer = bufnr,
-		desc = "LSP - Show type definition",
-	})
-	vim.keymap.set({ "n" }, "<leader>lr", vim.lsp.buf.rename, {
+	vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, {
 		buffer = bufnr,
 		desc = "LSP - Rename symbol",
 	})
-	vim.keymap.set({ "n" }, "<leader>la", vim.lsp.buf.code_action, {
-		buffer = bufnr,
-		desc = "LSP - Code actions",
-	})
-	vim.keymap.set({ "n" }, "<leader>lu", vim.lsp.buf.references, {
-		buffer = bufnr,
-		desc = "LSP - Show uses",
-	})
 	-- Diagnostics
-	vim.keymap.set({ "n" }, "[d", vim.diagnostic.goto_prev, {
+	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {
 		buffer = bufnr,
 		desc = "Diagnostic - Goto previous"
 	})
-	vim.keymap.set({ "n" }, "]d", vim.diagnostic.goto_next, {
+	vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {
 		buffer = bufnr,
 		desc = "Diagnostic - Goto next",
 	})
-	vim.keymap.set({ "n" }, "[e", function()
+	vim.keymap.set("n", "[e", function()
 		vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
 	end, {
 		buffer = bufnr,
 		desc = "Diagnostic - Goto previous error",
 	})
-	vim.keymap.set({ "n" }, "]e", function()
+	vim.keymap.set("n", "]e", function()
 		vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
 	end, {
 		buffer = bufnr,
 		desc = "Diagnostic - Goto next error",
 	})
 	-- Completion
-	vim.keymap.set({ "i" }, "<c-tab>", "<c-x><c-o>", {
+	vim.keymap.set("i", "<c-tab>", "<c-x><c-o>", {
 		buffer = bufnr,
 		desc = "LSP - Trigger omnifunc completion"
 	})
-	vim.keymap.set({ "i" }, "<tab>", function()
+	vim.keymap.set("i", "<tab>", function()
 		if vim.fn.pumvisible() == 1 then
 			return "<c-n>"
 		else
@@ -68,10 +75,10 @@ function M.setup(bufnr)
 		end
 	end, {
 		buffer = bufnr,
-		desc = "LSP - Next completion",
+		desc = "OmniFunc - Next completion",
 		expr = true,
 	})
-	vim.keymap.set({ "i" }, "<s-tab>", function()
+	vim.keymap.set("i", "<s-tab>", function()
 		if vim.fn.pumvisible() == 1 then
 			return "<c-p>"
 		else
@@ -79,7 +86,7 @@ function M.setup(bufnr)
 		end
 	end, {
 		buffer = bufnr,
-		desc = "LSP - Previous completion",
+		desc = "OmniFunc - Previous completion",
 		expr = true,
 	})
 end
