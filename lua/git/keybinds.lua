4b7653cd6eba642ee92d3e7d1c358d16a30f9fa6
Benjamin Betti <benjamin@betti.me>
2023-01-24 16:11:26 +0100

    feat(git): add unstage/reset keybinds

--- lua/git/keybinds.lua
+++ lua/git/keybinds.lua
@@ -14,9 +14,25 @@ function M.setup(bufnr)
 			buffer = bufnr,
 			desc = "Git - Stage buffer",
 		})
+		vim.keymap.set("n", "<leader>gub", gitsigns.reset_buffer_index, {
+			buffer = bufnr,
+			desc = "Git - Unstage buffer",
+		})
+		vim.keymap.set("n", "<leader>grb", gitsigns.reset_buffer_index, {
+			buffer = bufnr,
+			desc = "Git - Reset buffer",
+		})
 		vim.keymap.set("n", "<leader>gah", gitsigns.stage_hunk, {
 			buffer = bufnr,
 			desc = "Git - Stage hunk",
+		})
+		vim.keymap.set("n", "<leader>guh", gitsigns.undo_stage_hunk, {
+			buffer = bufnr,
+			desc = "Git - Unstage last hunk",
+		})
+		vim.keymap.set("n", "<leader>grh", gitsigns.reset_hunk, {
+			buffer = bufnr,
+			desc = "Git - Reset hunk",
 		})
 	end)
 end
