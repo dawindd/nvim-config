5f70861f2e4f1b25611fe3144b901419081b77f8
Benjamin Betti <benjamin@betti.me>
2023-02-14 11:14:33 +0100

    feat: add compose lang server

--- after/ftplugin/compose.lua
+++ after/ftplugin/compose.lua
@@ -1,18 +1,19 @@
-local cmd = { "vscode-css-language-server", "--stdio" }
+local cmd = { "docker-compose-langserver", "--stdio" }
 local root_files = {
 	".git",
+	"docker-compose.yml",
+	"docker-compose.yaml",
+	"compose.yml",
+	"compose.yaml",
 }
-local settings = {
-	css = {
-		validate = true,
-	}
-}
+local settings = {}
 
 local client = vim.lsp.start({
-	name = "css-lsp",
+	name = "compose-lsp",
 	cmd = cmd,
 	root_dir = require("lsp/utils").find_root(root_files),
 	capabilities = require("lsp/utils").get_capabilities(),
+	on_attach = require("commands/format").create_autocommand,
 	settings = settings,
 })
 
--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,7 +1,8 @@
-vim.api.nvim_create_autocmd({ "Filetype" }, {
+vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost", "Filetype" }, {
 	callback = function(args)
 		if vim.treesitter.highlighter.active[args.buf] ~= nil then
 			vim.cmd.normal("zR")
 		end
-	end
+	end,
+	group = vim.api.nvim_create_augroup("AutoUnfold", { clear = true })
 })
