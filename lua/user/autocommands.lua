e14bab7bc5ff4c67929d0e04121c6df14fbd459b
Benjamin Betti <benjamin@betti.me>
2023-02-19 19:08:34 +0100

    fix: make autounfold work again

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,6 +1,6 @@
-vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost", "Filetype" }, {
+vim.api.nvim_create_autocmd("BufWinEnter", {
 	callback = function()
 		vim.cmd.normal("zR")
 	end,
-	group = vim.api.nvim_create_augroup("AutoUnfold", { clear = true })
+	group = vim.api.nvim_create_augroup("AutoUnfold", { clear = true }),
 })
