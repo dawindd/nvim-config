fd520ec3124f7c6938ed39bbe5495619e23c34e2
Benjamin Betti <benjamin@betti.me>
2023-03-01 17:12:26 +0100

    feat: enable lsp formatting for css

--- after/ftplugin/css.lua
+++ after/ftplugin/css.lua
@@ -9,9 +9,10 @@ local settings = {
 local client = vim.lsp.start({
 	name = "css-lsp",
 	cmd = cmd,
-	root_dir = require("lsp/utils").find_root(root_files),
-	capabilities = require("lsp/utils").get_capabilities(),
+	root_dir = require("lsp.utils").find_root(root_files),
+	capabilities = require("lsp.utils").get_capabilities(),
 	settings = settings,
+	on_attach = require("commands.format").create_autocommand,
 })
 
 if client == nil then
