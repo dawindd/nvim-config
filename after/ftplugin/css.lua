5700dac6fc09bbb8962e1fa85ef83013eed08b3d
Benjamin Betti <benjamin@betti.me>
2023-03-04 13:19:38 +0100

    fix: disable css minification

--- after/ftplugin/css.lua
+++ after/ftplugin/css.lua
@@ -14,12 +14,11 @@ local client = vim.lsp.start({
 	root_dir = require("lsp.utils").find_root(root_files),
 	capabilities = require("lsp.utils").get_capabilities(),
 	settings = settings,
-	on_attach = require("commands.format").create_autocommand,
 })
 
 if client == nil then
 	vim.notify_once("Language server " ..
-	cmd[1] .. " not found, please check your system packages",
+		cmd[1] .. " not found, please check your system packages",
 		vim.log.levels.ERROR
 	)
 end
